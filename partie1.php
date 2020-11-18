82ca87e3c7070a595c7bdcbcaa540c332455baae
gouzien <johana.gouzien+lamanu@gmail.com>
2020-11-18 15:43:42 +0100

    exercices PHP partie1

---
+++ partie1.php
@@ -0,0 +1,38 @@
+<!-- Partie 1-->
+<html>
+<h1>Exercice 1</h1>
+<?php
+//Exercice 1 
+
+//On déclare les variables ( var object = "Hello";)
+$object="Hello";
+$message= "World";
+
+echo $object;
+echo $message;
+?>
+
+<h1>Exercice 2</h1>
+
+<?php
+//Exercice 2
+
+$lastname="Doe. ";
+$firstname="John ";
+$age="34 ";
+
+
+echo "Mon nom est ";
+echo $firstname;
+echo $lastname;
+echo "J'ai ";
+echo $age;
+echo "ans.";
+?>
+
+<h1>Exercice 3 </h1>
+<?php
+//Exercice 3
+
+?>
+</html>
